4cab78ab84ed393027c80967ea14e05190507569
Sallai Gergely <sallai.gergely@ustream.tv>
2016-02-08 16:48:13 +0100

    Call super.onFinishInflate()

--- lib/src/main/java/com/ms/square/android/expandabletextview/ExpandableTextView.java
+++ lib/src/main/java/com/ms/square/android/expandabletextview/ExpandableTextView.java
@@ -168,6 +168,7 @@ public class ExpandableTextView extends LinearLayout implements View.OnClickList
 
     @Override
     protected void onFinishInflate() {
+        super.onFinishInflate();
         findViews();
     }
 
